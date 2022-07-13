61b236b8650bdd59cd7d5b2af198570621832d6e
Abdul <s.abdulalhash@gmail.com>
2022-07-13 15:10:08 -0600

    Moving away from Bevy to Glium
    Bevy is still too primitive (pardon the pun)

--- tectonics/src/main.rs
+++ tectonics/src/main.rs
@@ -1,69 +1,33 @@
-use bevy::prelude::*;
-use bevy::render::mesh::{self, PrimitiveTopology};
-mod cam;
-mod light;
-/// set up a simple 3D scene
-
-fn make_mesh(
-            positions: Vec<[f32; 3]>, 
-            normals: Vec<[f32; 3]>, 
-            uvs: Vec<[f32; 2]>, 
-            indices: mesh::Indices
-)           -> bevy::prelude::Mesh {
-
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    mesh.set_indices(Some(indices));
-    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
-    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
-    mesh
-}
-
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    //setting vertices
-    let vertices = [
-        ([0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 1.0]),
-        ([1.0, 0.0, 1.0], [0.0, 1.0, 0.0], [1.0, 1.0]),
-        ([2.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 1.0]),
-    ];
-
-    //let indices = mesh::Indices::U32(vec![0, 2, 1, 0, 3, 2]);
-    let indices = mesh::Indices::U32(vec![1, 2, 0]);
-
-    let mut positions: Vec<[f32; 3]> = Vec::new();
-    let mut normals:   Vec<[f32; 3]> = Vec::new();
-    let mut uvs:       Vec<[f32; 2]> = Vec::new();
-
-    //adding position, normal, uvs to their corresponding vectors
-    for (position, normal, uv) in vertices.iter() {
-        positions.push(*position);
-        normals.push(*normal);
-        uvs.push(*uv);
-    }
-
-    //Mesh made up 
-    let mesh = make_mesh(positions, normals, uvs, indices);
-
-    // add entities to the world
-    // plane
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(mesh),
-        material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
-        ..default()
-    });
-}
+#[macro_use]
+extern crate glium;
 
 fn main() {
-    App::new()
-        .insert_resource(Msaa { samples: 4 })
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_startup_system(cam::spawn_camera)
-        .add_startup_system(light::point_light)
-        .add_system(cam::pan_orbit_camera)
-        .run();
+    use glium::{glutin, Surface};
+
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    event_loop.run(move |ev, _, control_flow| {
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+            },
+            _ => (),
+        }
+    });
 }
